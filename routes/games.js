4939167a10e9b10dd6d247159e4802799e5f4dbd
StudioBoloto <arseniijab@icloud.com>
2024-04-19 12:49:59 +0300

    Коммит после ревью

--- routes/games.js
+++ routes/games.js
@@ -1,10 +1,32 @@
 const gamesRouter = require("express").Router();
-const { sendAllGames, deleteGame, addGameController } = require("../controllers/game.js");
-const { getAllGames }= require("../middlewares/games.js")
+const {
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  findGameById,
+  deleteGame,
+} = require("../middlewares/games");
+const { sendAllGames, sendUpdatedGames } = require("../controllers/game.js");
 
-
-gamesRouter.post("/games", getAllGames, addGameController)
 gamesRouter.get("/games", getAllGames, sendAllGames);
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
+
+gamesRouter.post(
+  "/games",
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  sendUpdatedGames
+);
+
+gamesRouter.delete(
+  "/games/:id",
+  getAllGames,
+  findGameById,
+  deleteGame,
+  updateGamesFile,
+  sendUpdatedGames
+);
 
 module.exports = gamesRouter;
